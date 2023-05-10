8f840dbbd778af28b7e9b4ef07a6deb76b7fa8cf
cka <111500816+charlieUWUuwu@users.noreply.github.com>
2023-05-10 23:58:56 +0800

    Update Identify_img.py
    即時抓取鏡頭影像並辨識

--- Identify_img.py
+++ Identify_img.py
@@ -1,29 +1,66 @@
 # Identify_img.py
 import cv2
 from DialReader import DIAL
-import gc
 import time
+import threading
 
-if __name__ == '__main__':
-  
-  img_full = cv2.imread('output.jpg')
+def process_image(img, sec_count):
+    obj = DIAL(img)
+    imgs, values = obj.get_pointer_value()
 
-  start = time.time()
-  obj = DIAL(img_full)
-  results, values = obj.get_pointer_value()
+    # 由左至右的錶盤
+    print("sec_count: " + str(sec_count))
+    print(values[0])
+    print(values[1])
+    print(values[3])
+    print(values[2])
 
-  for i in range(len(results)):
+if __name__=='__main__':
+    rtsp_url = "rtsp://justin:jufan2534879@210.61.41.223:554/stream1"
 
-    print(values[i])
-    # 顯示圖片
-    # cv2.imshow('results', results[i])
+    # 創建VideoCapture對象
+    cap = cv2.VideoCapture(rtsp_url)
 
-    # 按下任意鍵則關閉所有視窗
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    
-  del img_full
-  gc.collect()
+    # 設置捕獲視頻的畫面大小
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-  end = time.time()
-  print("花費時間:"+str(end-start)+"秒")
+    # 設置每10秒為一個間隔捕獲一張圖像
+    interval = 10
+
+    # 定義保存圖像的文件夾路徑
+    save_path = './image/'
+
+    count = 0
+    timeStart = int(time.time())
+    while True:
+        # 讀取RTSP影像
+        ret, frame = cap.read()
+        
+        if ret:
+            sec_count = int(time.time()-timeStart)
+            print('sec:', sec_count , ' success')
+            # 每interval秒捕獲一張圖像
+            if sec_count % interval == 0:
+                
+                # 生成文件名
+                filename = time.strftime("image") + str(sec_count) + ".jpg" 
+                # 拼接文件路徑
+                file_path = save_path + "/" + filename
+
+                # 保存圖像
+                cv2.imwrite(file_path, frame)
+
+                # 同時處理圖片
+                t = threading.Thread(target=process_image, args=(frame,sec_count,), daemon=True)  # 設定為daemon thread
+                t.start()  #啟動
+
+                count += 1
+        else:
+            # 若讀取失敗，則重新連接
+            print('error')
+            cap.release()
+            cap = cv2.VideoCapture(rtsp_url)
+
+        # 暫停1秒
+        time.sleep(1)
